6a4bacb206abf10ca317bb75d096f242e71eb207
Daniel Nichols <info.danielnichols@gmail.com>
2020-08-10 18:13:34 +1000

    added page routing
    Includes a couple of placeholder pages, a 404 redirect, and a redirect to coming soon when in production

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter, Switch, Route, Redirect,
+} from 'react-router-dom';
 
 import ComingSoon from '~components/ComingSoon';
 
+const isDev = process.env.NODE_ENV !== 'production';
+
 const App = () => (
-  <div>
-    <ComingSoon />
-  </div>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact>
+        {
+          !isDev
+            ? <Redirect to="/soon" />
+            : <p>Homepage</p>
+        }
+      </Route>
+      <Route path="/soon" exact>
+        <ComingSoon />
+      </Route>
+      <Route path="/404">
+        <p>Page not found.</p>
+      </Route>
+      <Route>
+        <Redirect to="/404" />
+      </Route>
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
