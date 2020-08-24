20ed445a8095c76a82b251e15a24ec12b8218f3e
Daniel Nichols <info.danielnichols@gmail.com>
2020-08-24 17:59:31 +1000

    added a test page

---
+++ src/pages/testPage.ts
@@ -0,0 +1,15 @@
+import type { PageData } from '~components/PortfolioItemPage';
+
+const data: PageData = {
+  title: 'Test Page',
+  path: 'testpage',
+  summary: {
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac condimentum libero. Nulla neque metus, sollicitudin sit amet euismod quis, vestibulum vitae metus.',
+  },
+  content: {
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac condimentum libero. Nulla neque metus, sollicitudin sit amet euismod quis, vestibulum vitae metus.',
+    attribution: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  },
+};
+
+export default data;
