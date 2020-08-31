f87e5fa3b7b6275bb4d069e995739fd362462694
Daniel Nichols <info.danielnichols@gmail.com>
2020-08-31 18:39:32 +1000

    added option to center background in parallax background

--- src/components/common/Parallax/ParallaxBackground.tsx
+++ src/components/common/Parallax/ParallaxBackground.tsx
@@ -6,16 +6,20 @@ import styled from 'styled-components';
 const Background = styled(animated.div)`
   background: url(${({ backgroundURL }) => backgroundURL});
   position: fixed;
-  width: 100vw;
+  width: ${({ width }) => width}px;
   height: ${({ height }) => height + 600}px;
+  background-position-x: ${({ center }) => (center ? 'center' : '')};
 `;
 
 interface ParallaxBackgroundProps {
   backgroundURL: string;
+  backgroundOffsetX?: number;
+  /** Aligns the background to the center of the component. Often used with x offset to center background content as well */
+  centerBackground?: boolean;
 }
 
 const ParallaxBackground: React.FC<ParallaxBackgroundProps> = props => {
-  const [ref, { height: contentHeight }] = useMeasure();
+  const [ref, { height: contentHeight, width: contentWidth }] = useMeasure();
   const { y: scrollY } = useWindowScroll();
   const [{ scrollTop }, setSpring] = useSpring(() => ({
     scrollTop: -600,
@@ -31,6 +35,8 @@ const ParallaxBackground: React.FC<ParallaxBackgroundProps> = props => {
       <Background
         backgroundURL={ props.backgroundURL }
         height={ contentHeight }
+        width={ contentWidth + (props.backgroundOffsetX ?? 0) }
+        center={ props.centerBackground }
         style={ { transform: scrollTop.interpolate(y => `translateY(${y}px)`) } }
       />
       <div ref={ ref }>
