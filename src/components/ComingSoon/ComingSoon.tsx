76aa39c75d038b240bf551d6f045fa99a95b56c9
Daniel Nichols <info.danielnichols@gmail.com>
2020-09-08 00:20:44 +1000

    improved coming soon page

--- src/components/ComingSoon/ComingSoon.tsx
+++ src/components/ComingSoon/ComingSoon.tsx
@@ -1,38 +1,50 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const Wrapper = styled.div`
-  text-align: center;
-`;
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import DotGrid from '~assets/backgrounds/diagonal_striped_brick.png?size=2000';
 
 const Link = styled.a`
-  color: white;
+  color: #b84747;
+  text-shadow: -1px -1px 0 #fee, 1px -1px 0 #fee, -1px 1px 0 #fee, 1px 1px 0 #fee;
+`;
+
+const scroll = keyframes`
+from {
+  background-position: 0 0;
+}
+
+to {
+  background-position: -300px -300px;
+}
 `;
 
 const Header = styled.header`
-  background-color: #b84747;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   font-size: 30px;
-  color: white;
+  color: #b84747;
+  background: url(${DotGrid.src});
+  background-color: ${({ theme }) => theme.colors.reds[1]};
+  background-blend-mode: luminosity;
+  animation: ${scroll} 20s linear infinite;
 `;
 
 const ComingSoon = () => (
-  <Wrapper>
-    <Header>
-      <h1>
-        Coming Soon
-      </h1>
-      <p>
-        See progress on
-        {' '}
-        <Link href="https://github.com/danielnichols/danielnichols.github.io">Github</Link>
-      </p>
-    </Header>
-  </Wrapper>
+  <Header>
+    <h1>
+      Coming Soon
+    </h1>
+    <p>
+      See progress on
+      {' '}
+      <Link href="https://github.com/danielnichols/danielnichols.github.io">Github</Link>
+    </p>
+  </Header>
 );
 
 export default ComingSoon;
