60b6d7affb89b44230cf8f90cf75225fa5b8e2ef
Daniel Nichols <contact@danielnichols.me>
2020-09-20 16:06:28 +1000

    card title should overlap image when image exists

--- src/components/common/Timeline/TimelineCard/CardTitle.tsx
+++ src/components/common/Timeline/TimelineCard/CardTitle.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TitleContainer = styled.div`
+  position: absolute;
+  padding-left: 6px;
   text-align: left;
+  font-size: ${({ theme }) => theme.fontSizes[4]}px;
 `;
 
 const CardTitle = props => (
