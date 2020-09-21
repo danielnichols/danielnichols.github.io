d2961b0291ff57fcd5642f33eed30830019330ca
Daniel Nichols <contact@danielnichols.me>
2020-09-21 19:18:31 +1000

    cleaned up font stack function

--- src/theme/utils.ts
+++ src/theme/utils.ts
@@ -2,4 +2,6 @@
  * Takes an array of fonts and creates a list where entries with spaces are quoted
  * @param fonts
  */
-export const createFontSet = (fonts: string[]) => fonts.map(font => (font.includes(' ') ? `"${font}"` : font)).join(',');
+export const createFontSet = (fonts: string[]) => fonts
+  .map(font => (font.includes(' ') ? `"${font}"` : font))
+  .reduce((prev, font) => `${prev}, ${font}`);
